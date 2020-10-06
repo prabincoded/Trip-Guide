260ed8747dc283bb5282f1bf8fdd310c8e1ff2c3
Lokindra Dangi <lokendradangi0@gmail.com>
2020-10-06 22:29:15 +0545

    comparing params url and place id to fetch place data

--- src/components/PlacesDetails.js
+++ src/components/PlacesDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./PlacesDetails.css";
 import { useParams } from "react-router-dom";
+import data from "../data";
 function PlacesDetails(props) {
   const { id } = useParams();
   console.log(id);
+  const place = data.places.find((x) => x._id === id);
   return (
     <div className="placesDetails">
-      <h2>Now showing post</h2>
+      <h2>{place.title}</h2>
     </div>
   );
 }
